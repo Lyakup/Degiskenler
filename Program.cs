a835773ce1ae283394d0ce886958db62f3a1ced7
Yakup Layık <yakuplayik@hotmail.com>
2022-01-25 09:34:22 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 22;
+            int i = 1;
+            Console.WriteLine(i);
+        }
+    }
+}
