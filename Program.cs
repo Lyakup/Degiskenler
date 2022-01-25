4af5a449be6e0a9933d09fdd67484492692f9f42
Yakup Layık <yakuplayik@hotmail.com>
2022-01-25 09:41:04 +0300

    değişkenler eklendi

--- Program.cs
+++ Program.cs
@@ -7,8 +7,27 @@ namespace Degiskenler
         static void Main(string[] args)
         {
             byte b = 22;
-            int i = 1;
-            Console.WriteLine(i);
+            sbyte sb = 1;
+
+            short s = 2;
+            ushort us = 3;
+
+            Int16 i = 4;
+            int j = 5;  
+            Int32 i2 = 6;
+            Int64 j2 = 7;
+
+            uint u2 = 8;
+            long l = 9;
+            ulong ul = 10;
+
+            float f = 1.1f;
+            double d = 12.13;
+            decimal dec = 43;
+
+            char c = 'c';
+            string s = "yakup";
+
         }
     }
 }
